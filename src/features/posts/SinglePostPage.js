970dde14f11001ea1614c185e7cd9ef0ee21f912
Jose <jloai009@fiu.edu>
2022-02-15 01:58:00 -0500

    Add feature: posts have authors

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 
+import { Link } from 'react-router-dom'
+
+import { PostAuthor } from './PostAuthor'
+
 export const SinglePostPage = ({ match }) => {
   const { postId } = match.params
 
@@ -20,7 +24,11 @@ export const SinglePostPage = ({ match }) => {
     <section>
       <article className="post">
         <h2>{post.title}</h2>
+        <PostAuthor userId={post.user} />
         <p className="post-content">{post.content}</p>
+        <Link to={`/editPost/${post.id}`} className="button">
+          Edit Post
+        </Link>
       </article>
     </section>
   )

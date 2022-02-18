d88cd41c19f2200edb420c9a8fdfdeace0d10548
Jose <jloai009@fiu.edu>
2022-02-18 00:48:43 -0500

    Use memoized components & selectors to avoid unnecessary rerendering and improve performance

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -1,20 +1,27 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 import {
   fetchNotifications,
-  allNotificationsRead,
+  selectAllNotifications,
 } from '../features/notifications/notificationsSlice'
 
 export const Navbar = () => {
   const dispatch = useDispatch()
+  const notifications = useSelector(selectAllNotifications)
+  const numUnreadNotifications = notifications.filter((n) => !n.read).length
 
   const fetchNewNotifications = () => {
-    if (window.location.pathname === '/notifications') {
-      dispatch(allNotificationsRead())
-    }
     dispatch(fetchNotifications())
+  }
+
+  let unreadNotificationsBadge
+
+  if (numUnreadNotifications > 0) {
+    unreadNotificationsBadge = (
+      <span className="badge">{numUnreadNotifications}</span>
+    )
   }
 
   return (
@@ -26,7 +33,9 @@ export const Navbar = () => {
           <div className="navLinks">
             <Link to="/">Posts</Link>
             <Link to="/users">Users</Link>
-            <Link to="/notifications">Notifications</Link>
+            <Link to="/notifications">
+              Notifications {unreadNotificationsBadge}
+            </Link>
           </div>
           <button className="button" onClick={fetchNewNotifications}>
             Refresh Notifications
